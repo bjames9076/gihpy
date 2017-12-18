746f6af4fa35338ba3dec7f1c61c74cf9b49cca8
Brandon James <bjames9076@gmail.com>
2017-12-17 19:34:28 -0500

    added js

---
+++ assets/javascript/app.js
@@ -0,0 +1,43 @@
+function search(){
+    var name = document.getElementById("searchTerm").value;
+    var btn = document.createElement("BUTTON");
+    btn.id = name;
+    btn.value = name;
+    btn.className = "button";
+    var t = document.createTextNode(name);
+    btn.appendChild(t);
+    document.getElementById("buttons").appendChild(btn);
+    
+}
+//fill html with giphy
+function populate(){
+    var person = $(".button").attr("value");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      person + "&api_key=dc6zaTOxFJmzC&limit=10";
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+      .done(function(response) {
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+          var gifDiv = $("<div class='item'>");
+
+          var rating = results[i].rating;
+
+          var p = $("<p>").text("Rating: " + rating);
+
+          var personImage = $("<img>");
+          personImage.attr("src", results[i].images.fixed_height.url);
+
+          gifDiv.prepend(p);
+          gifDiv.prepend(personImage);
+
+          $("#gifs-appear-here").prepend(gifDiv);
+          
+        }
+      });
+  
+}
